e6e608fb269baa65d7a6e4260cfab3612fd9b032
Nicolas Andres Zarate <zarateguarinnicolasandres@gmail.com>
2022-09-26 22:39:06 -0500

    Dates

--- ECMAScript6/src/es9/index.js
+++ ECMAScript6/src/es9/index.js
@@ -88,3 +88,11 @@ promesa().then(response => console.log(response)) // Promesa resuelta
 // Las expresiones regulares o RegEx (regular expresions) son patrones de búsqueda y manipulación de cadenas de caracteres increíblemente potente y están presentes en todos los lenguajes de programación. En JavaScript se crea estre patrón entre barras inclinadas (/patrón/) y se utiliza métodos para hacer coincidir la búsqueda.
 const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
 const match = regexData.exec('2018-04-20')
+
+const idk = match[0]
+const year = match[1]
+const month = match[2]
+const day = match[3]
+
+console.log(year, month, day);
+console.log(idk);
