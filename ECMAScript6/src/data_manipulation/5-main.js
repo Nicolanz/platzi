25cf35314675b84fa5894a88e3b4b1abe030c1c6
Nicolas Andres Zarate <zarateguarinnicolasandres@gmail.com>
2022-10-21 21:31:54 -0500

    another day

--- ECMAScript6/src/data_manipulation/5-main.js
+++ ECMAScript6/src/data_manipulation/5-main.js
@@ -3,4 +3,5 @@ import createInt8TypedArray from "./5-typed_arrays.js";
 console.log("Obj created")
 console.log("Next generation")
 console.log('People')
+console.log('Another one')
 console.log(createInt8TypedArray(10, 2, 89));
